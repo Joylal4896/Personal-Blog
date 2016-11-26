c04b146ce17124b75862bbcf8df28666f6bf44ea
Joy Lal Chattaraj <joylal4896@gmail.com>
2016-11-26 16:00:54 +0000

    Comit before editing puma.rb

---
+++ app/views/joys/_joy.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! joy, :id, :name, :address, :created_at, :updated_at
+json.url joy_url(joy, format: :json)
---
+++ db/migrate/20161126150359_create_joys.rb
@@ -0,0 +1,10 @@
+class CreateJoys < ActiveRecord::Migration[5.0]
+  def change
+    create_table :joys do |t|
+      t.string :name
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
